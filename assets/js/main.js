19f7617a03df719f9a1758bfd0b95f373c3ace31
Marina <59148715+Fuga29@users.noreply.github.com>
2020-04-26 15:06:12 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,5 +62,13 @@
 					.css('transition', 'none');
 
 	});
+	
+		$('#myCarousel').carousel({
+	interval: 0
+	})
+    
+    $('#myCarousel').on('slid.bs.carousel', function() {
+    	alert("slid");
+	});
 
 })(jQuery);
